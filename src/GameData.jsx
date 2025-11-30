71f2b55870d9f2863a0f11debb529a1e05128574
Martin Frei <mat.frei@gmx.de>
2025-11-30 12:53:35 +0100

    Fix image paths

--- src/GameData.jsx
+++ src/GameData.jsx
@@ -1,13 +1,12 @@
-
 const gameData = [
-    './img1.jpg',    
-    './img2.jpg',
-    './img3.jpg',
-    './img4.jpg',
-    './img1.jpg',    
-    './img2.jpg',
-    './img3.jpg',
-    './img4.jpg',
+    `${process.env.PUBLIC_URL}/img1.jpg`,
+    `${process.env.PUBLIC_URL}/img2.jpg`,
+    `${process.env.PUBLIC_URL}/img3.jpg`,
+    `${process.env.PUBLIC_URL}/img4.jpg`,
+    `${process.env.PUBLIC_URL}/img1.jpg`,
+    `${process.env.PUBLIC_URL}/img2.jpg`,
+    `${process.env.PUBLIC_URL}/img3.jpg`,
+    `${process.env.PUBLIC_URL}/img4.jpg`,
 ]
 
 export default gameData;
